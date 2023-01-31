4480a0698bc5dda1f9e4c7af62fdd246e151c053
crisjulyancolesco <crisjulyancolesco@iskolarngbayan.pup.edu.ph>
2023-01-31 17:03:55 +0800

    Push and Pop operations done (display operation not done)

--- Stacks.py
+++ Stacks.py
@@ -1,9 +1,40 @@
+import numpy as geek
+
 # STACKS AND ITS APPLICATION
+Size = 7
+Stack = [0] * Size
+Top = -1
+
+# PUSH - add a new element into the stack
+def Push(Data):
+    global Top
+    if Top == Size - 1:
+        print("The stack is in overflow condition.")
+    
+    if Top != Size - 1:
+        Top = Top + 1
+        Stack[Top] = Data
+        print(f"The Number {Data} is pushed on the stack")
+
+def Pop():
+    global Top
+    if Top > 0:
+        Data = Stack[Top]
+        Top = Top - 1
+        print(f"The Number {Data} is popped on the stack.")
+        return Data
+    else:
+        print("The stack is in underflow condition.")
+
+def Display():
+    Value = Stack[geek.nonzero(Stack)]
+    print(Value)
 
 def MainMenu():
     global UserInput
-    print("")
-    print("""***************************************
+    while True:
+        print("")
+        print("""***************************************
 *****  My Stack Menu              *****
 *****  1. Push                    *****
 *****  2. Pop                     *****
@@ -11,11 +42,11 @@ def MainMenu():
 *****  4. Exit                    *****
 ***************************************""")
 
-    UserInput = int(input("Enter your choice: "))
-
-    while True:
+        UserInput = int(input("Enter your choice: "))
+        
         if UserInput == 1:
-            Push()
+            El = int(input("Enter the number to be pushed into the stack: "))
+            Push(El)
         
         if UserInput == 2:
             Pop()
@@ -26,9 +57,5 @@ def MainMenu():
         if UserInput == 4:
             break
         
-        else:
-            print("Invalid Input!")
-            return MainMenu()
-
 # Start the Program
 MainMenu()
